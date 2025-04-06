9c159fd7e998085a7a44fb580566a8336bf457bf
animesh-1998 <animesh.uit1998@gmail.com>
2025-04-06 17:15:47 +0530

    Intial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will analyze the comment of youtube videos in real time.',
+    author='animesh.uit1998@gmail.com',
+    license='MIT',
+)
