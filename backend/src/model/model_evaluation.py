cfc8d876f019fbbb60b607ce6218be4a41c82107
animesh-1998 <animesh.uit1998@gmail.com>
2025-04-25 17:57:20 +0530

    Testing cicd pipeline

--- backend/src/model/model_evaluation.py
+++ backend/src/model/model_evaluation.py
@@ -128,7 +128,7 @@ def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
 
 
 def main():
-    mlflow.set_tracking_uri("http://ec2-13-49-80-49.eu-north-1.compute.amazonaws.com:5000/")
+    mlflow.set_tracking_uri("http://ec2-13-51-173-140.eu-north-1.compute.amazonaws.com:5000/")
 
     mlflow.set_experiment('dvc-pipeline-runs')
     
